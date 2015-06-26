de0d1f432f0985e56500185ab8c57c7479d3c193
Trent Niemeyer <trent@peaksware.com>
2015-06-25 20:31:38 -0600

    display image with search

--- _site/js/app.js
+++ _site/js/app.js
@@ -18,6 +18,20 @@ window.Calaca = angular.module('calaca', ['elasticsearch', 'ngAnimate'],
             return $sce.trustAsHtml(text);
         };
     }]
-);
+).directive('onErrorSrc', function() {
+    return {
+        link: function(scope, element, attrs) {
+          element.bind('error', function() {
+            if (attrs.src != attrs.onErrorSrc) {
+              attrs.$set('src', attrs.onErrorSrc);
+            }
+          });
+        }
+    }
+});
+
+
+
+
 
 
